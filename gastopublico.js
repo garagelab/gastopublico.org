49274ccf86975e660a37e10866cc9c88e46772d9
Manuel Aristarán <jazzido@jazzido.com>
2013-06-28 00:31:18 -0300

    ajustes

--- gastopublico.js
+++ gastopublico.js
@@ -8,8 +8,8 @@ $(function() {
     var ready = function(error, partidos) {
 
         var feature = topojson.feature(partidos, partidos.objects.partidos);
-        var width = 800, height = 800;
-        var svg = d3.select("#wrapper").append("svg")
+        var width = 600, height = 600;
+        var svg = d3.select("#mapa").append("svg")
             .attr("width", width)
             .attr("height", height);
         var projection = d3.geo.mercator()
@@ -33,7 +33,7 @@ $(function() {
 
         partidos_g.attr('transform',
                         'translate(' + projection.translate() + ')'
-                        + 'scale(' + 1 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ')'
+                        + 'scale(' + (1 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height))  + ')'
                         + "translate(" + -(b[1][0] + b[0][0]) / 2 + "," + -(b[1][1] + b[0][1]) / 2 + ")");
 
         $('#sitios li').each(function(s) {
@@ -41,8 +41,29 @@ $(function() {
         });
 
         $('.partidos').on('mouseover', 'path', function() {
-            console.log(this);
+            $('#sitios li[data-partido="'+ $(this).attr('id') +'"]')
+            .css('background-color', '#ddd');
+        })
+        .on('mouseout', 'path', function() {
+            $('#sitios li[data-partido="'+ $(this).attr('id') +'"]')
+                .css('background-color', 'white');
+        })
+        .on('click', 'path', function() {
+            var a = $('#sitios li[data-partido="'+ $(this).attr('id') +'"] a');
+            console.log(a[0]);
+            a.trigger('click');
         });
+
+        $('#sitios li').on('mouseover', function() {
+            $('path#' + $(this).data('partido'))
+                .css('fill-opacity', 0.4)
+                .css('stroke', '#000');
+        })
+        .on('mouseout', function() {
+            $('path#' + $(this).data('partido')).css('fill-opacity', 1);
+        });
+
+
     };
 
 
